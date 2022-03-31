7456289a02808717032fc09cafaae556d8cf9190
Amin Sabbagh <71517831+Amin-Sabbagh@users.noreply.github.com>
2022-03-31 14:40:24 +0100

    index.php psudocode
    expanded on the psudocode on how the website should function

--- index.php
+++ index.php
@@ -1,9 +1,38 @@
 <?php
-///include header
+///include header (which includes the navigation and viewport)
 
-//test sql here
+//all users have access to this page
 
-//should be able to sort view of sql results
+// a scroll down form should be presented here to access the different if statements
+// the form should do this by having diffferent ids to each optiom
+// i.e id = "ratings" would be if user chooses ratings option
+
+// there should be at least 7 different options to view the films which are:
+
+    //genre 
+    //selected year of release
+    //budget range
+    //generated revenue within a selected range
+    //selected actors
+    //selected director
+
+    //this could be implemented as a search bar
+    //specific film title, actor or director (i would go with title)
+
+//These ifs trigger different sql statements, which all have a where clause
+// these tables should also have and inner or outer joing depending on the id sent by the form
+// if nothing is selected, use an else statment for a defualt select statment of the results
+
+//the results should be viewed as cards or tables (no borders) to emmulate as much
+// -as the orginal design daniel and faizaan had in mind
+//relevant information should be shown, such as pictures, ratings, titles
+//buttons should be available to see more information on each
+            
+    //all users should see a button to see the film's own webpage
+    //links to different websites (extra)
+    //if users are logged in a wishlist button should be available 
+    
+
 
 //include footer
 ?>
