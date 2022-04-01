9f907e64f5489ab5dd2411ef4797e717e21c2d1e
Syed Kashan <102334086+Syed200@users.noreply.github.com>
2022-04-01 13:00:43 +0100

    Signup.php
    signup.php psuedo code,
    
    SYED

--- signup.php
+++ signup.php
@@ -1,3 +1,39 @@
 <?php
 //form to sign up based on sql and erd
+
+//include header
+//include header (which includes the navigation and viewport)
+
+
+
+//all user visiting the website should be able to access this page
+
+
+
+//signup form ## Entities to include
+
+//FirstName field: input type = First Name, placeholder: "Enter First Name"
+//LastName field: input type = Last Name, placeholder: "Enter Last Name
+//UserName field: input type = Username, placeholder: "Enter username"
+//Email field: input type = Email, placeholder: "Enter your@email.com"
+//Password field: input type = Password, placeholder: "Enter Password"
+//Password confirmation field: input type = Password , placeholder: "Re-enter Password"
+
+
+//IF username entered already in use the ERROR message: "username already in use, Please use a different username"
+//ELSE IF username entered already in use
+
+//IF any field is left blank then the script should display an error message saying to enter + empty field
+//IF any field the information entered is not valid then error message: "field + 'not valid' "
+
+
+
+
+//Email field possible errors
+//IF email field is left blank then error message: "Enter email"
+//ELSE IF email entered is already in use then ERROR message: "Email already in use, Please Sign IN"
+//ELSE IF email contains values special characters (?, <, >, !, £, $, %, &, *, ^, #, etc.) 
+//then ERROR message email address not valid, Please try again.
+
+
 ?>
