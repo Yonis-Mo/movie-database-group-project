c82a7fc1bc1235441979b7ef2bd38ff003cbbea4
ChickenBoy1 <liamsingh64@gmail.com>
2022-04-01 14:19:28 +0100

    Can Find Certain Genres

--- sql/select_film_genres.php
+++ sql/select_film_genres.php
@@ -3,14 +3,27 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "moviesdatabase";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+$sql = "SELECT * FROM movies WHERE Genres LIKE ?;";
+$stmt = $conn->prepare($sql); 
+$search_genre = "%Animation%";
+$stmt->bind_param("s", $search_genre);
+$id = 1;
+
+$stmt->execute();
+$result = $stmt->get_result();
+
+while ($row = $result->fetch_assoc()) {
+    echo $row['Title'];
+}
 
 ?>
+
