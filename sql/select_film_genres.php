810220952bdb25e02fb991fd58430348c6e4ac1e
ChickenBoy1 <liamsingh64@gmail.com>
2022-04-02 19:00:23 +0100

    Make a table of movie info from specific genre
    UNTESTED BCOS OF APACHE SERVER PROBLEMS

--- sql/select_film_genres.php
+++ sql/select_film_genres.php
@@ -21,9 +21,39 @@ $id = 1;
 $stmt->execute();
 $result = $stmt->get_result();
 
+$table = <<<TABLE
+<table>
+  <th>Title</th>
+  <th>Overview</th>
+  <th>Budget</th>
+  <th>Revenue</th>
+  <th>Release Date</th>
+  <th>Runtime</th>
+  <th>Average Vote</th>
+  <th>Popularity</th>
+
+TABLE;
+
+echo $table;
+
 while ($row = $result->fetch_assoc()) {
-    echo $row['Title'];
+  
+    echo "<tr>";
+    echo "<td>".$row['Title']."</td>";
+    echo "<td>".$row['Overview']."</td>";
+    echo "<td>".$row['Budget']."</td>";
+    echo "<td>".$row['Revenue']."</td>";
+    echo "<td>".$row['ReleaseDate']."</td>";
+    echo "<td>".$row['Runtime']."</td>";
+    echo "<td>".$row['VoteAvg']."</td>";
+    echo "<td>".$row['Popularity']."</td>";
+    echo "</tr>";
+
 }
+
+echo "</table>";
+
+echo $table;
 
 ?>
 
