2f8bf9fb27da372db4f7422ec12098358b7d0c00
Amin Sabbagh <71517831+Amin-Sabbagh@users.noreply.github.com>
2022-04-03 18:53:22 +0100

    intial javascript

--- js/dom.js
+++ js/dom.js
@@ -1,2 +1,57 @@
 // javascript file
 // darkmode and accessiblity should be coded
+
+// uncomment these when we start implementing javascript ajax and jquery
+
+// function changeTheme() {
+//     href = document.getElementById("stylesheet").getAttribute("href");
+//     currentColour = href.includes("dark");
+
+//     styleLink = "";
+
+//     if (!currentColour) {
+//         styleLink = "css/taskit_dark.css";
+//         $("#stylesheet").attr("href", "css/taskit_dark.css");
+//         $("#theme_toggle").text("Dark Mode");
+
+//     } else {
+//         $("#stylesheet").attr("href", "css/taskit.css");
+//         $("#theme_toggle").text("Light Mode");
+//     }
+
+// }
+
+// $(document).ready(function () { // select the dropdown and action when it changes
+//     $("#backColor").change(function () {
+//         var backColor = $(this).val();
+//         $("body, header").css("background-color", backColor);
+//     })
+
+//     $("#fontSelector").change(function () {
+//         var newFont = $(this).val();
+//         $("body, h1, h2, h3, h4, h5, h6, p, a, button, input, select").css("font-family", newFont);
+
+//     })
+
+//     $("#fontColor").change(function () {
+//         var newColor = $(this).val();
+//         $("body, h1, h2, h3, h4, h5, h6, p, a, label, #time").css("color", newColor);
+
+//     })
+
+//     $("#fontSize").change(function () {
+//         var newSize = $(this).val();
+//         $("#body_text, h1, h2, h3, h4, h5, h6").css("font-size", newSize);
+
+//     })
+
+// });
+
+// function showPassword() {
+//     var pw = document.getElementById("password");
+//     if (pw.type === "password") {
+//         pw.type = "text";
+//     } else {
+//         pw.type = "password";
+//     }
+// }
