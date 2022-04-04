687e62a8c48a882cf3fbddc44597fd3f15aaa5aa
ChickenBoy1 <liamsingh64@gmail.com>
2022-04-04 21:06:39 +0100

    Create select_films_by_money.php

---
+++ server-side/select_films_by_money.php
@@ -0,0 +1,74 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "moviesdatabase";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$search_by = $_GET['search_by'];    //"budget" or "revenue"
+$max = $_GET['max'];
+$min = $_GET['min'];
+
+
+if($search_by == "budget"){
+    //SEARCH FOR FILMS WITHIN SPECIFIC BUDGET
+    $sql = "SELECT * FROM movies WHERE Budget < ? AND Budget > ?;";
+}else{
+    //SEARCH FOR FILMS WITHIN SPECIFIC REVENUE
+    $sql = "SELECT * FROM movies WHERE Revenue < ? AND Budget > ?;";
+}
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ii", $max, $min);
+
+$stmt->execute();
+$result = $stmt->get_result();
+
+$table = <<<TABLE
+<table>
+  <th>Title</th>
+  <th>Overview</th>
+  <th>Budget</th>
+  <th>Revenue</th>
+  <th>Release Date</th>
+  <th>Runtime</th>
+  <th>Average Vote</th>
+  <th>Popularity</th>
+
+TABLE;
+
+
+if($result->num_rows > 0){
+  echo $table;
+while ($row = $result->fetch_assoc()) {
+  
+    echo "<tr>";
+    echo "<td>".$row['Title']."</td>";
+    echo "<td>".$row['Overview']."</td>";
+    echo "<td>".$row['Budget']."</td>";
+    echo "<td>".$row['Revenue']."</td>";
+
+    $UNIX_DATE = ($row['ReleaseDate'] - 25569) * 86400;
+    $readable_date = gmdate("d-m-Y", $UNIX_DATE);
+    echo "<td>".$readable_date."</td>";
+    echo "<td>".$row['Runtime']."</td>";
+    echo "<td>".$row['VoteAvg']."</td>";
+    echo "<td>".$row['Popularity']."</td>";
+    echo "</tr>";
+
+}
+}else{
+  echo "Sorry! We couldn't find any films of that genre";
+}
+
+echo "</table>";
+
+?>
+
