7f141e06e2e2fcc45cbda9433f1c55309e3097d4
JusFaizaan <62467394+JusFaizaan@users.noreply.github.com>
2022-04-06 11:35:49 +0100

    forms
    me and Amin create some forms

--- index.php
+++ index.php
@@ -1,25 +1,66 @@
 <?php
 include 'header.php';
 
-echo '<h1> website name here! </h1>';
-
-
+echo '<h1> The A-Team Movie Website </h1>';
 
 echo <<<_END
 <br>
 <div class= "sort">
 <form action="index.php"  method="post">
-  <label for="choice">Sorting Options: </label> <br>
+  <label for="choice">Choose a Genre: </label> <br>
   <select id="choice" name="choice" >
     <option> Select your option</option>
-    <option value="#" name="#">1</option>
-    <option value="#" name="#">2</option>
-    <option value="#" name="#">3</option>
-    <option value="#" name="#">4</option>
+    <option value="action" name="Action">Action</option>
+    <option value="romance" name="Romance">Romance</option>
+    <option value="thriller" name="Thriller">Thriller</option>
+    <option value="horror" name="Horror">Horror</option>
   </select>
   <button type="submit" value="Submit"> Sort</buttn>
 </form>
 </div>
+
+<div class= "sort">
+<form action="index.php"  method="post">
+  <label for="choice">Choose a Year: </label> <br>
+  <select id="choice" name="choice" >
+    <option> Select your option</option>
+    <option value="1970s" name="1970s">1970-1980</option>
+    <option value="1980s" name="1980s">1980-1990</option>
+    <option value="1990s" name="1990s">1990-2000</option>
+    <option value="2000s" name="2000s">2000-2010</option>
+  </select>
+  <button type="submit" value="Submit"> Sort</buttn>
+</form>
+</div>
+
+<div class= "sort">
+<form action="index.php"  method="post">
+  <label for="choice">Choose a Movie Budget: </label> <br>
+  <select id="choice" name="choice" >
+    <option> Select your option</option>
+    <option value="very_cheap" name="very_cheap">$0-$100,000</option>
+    <option value="cheap" name="cheap">$100,000-$500,000</option>
+    <option value="median" name="median">$500,000-$1,000,000</option>
+    <option value="expensive" name="expensive">$1,000,000-$10,000,000</option>
+  </select>
+  <button type="submit" value="Submit"> Sort</buttn>
+</form>
+</div>
+
+<div class= "sort">
+<form action="index.php"  method="post">
+  <label for="choice">Choose a Profit Margin: </label> <br>
+  <select id="choice" name="choice" >
+    <option> Select your option</option>
+    <option value="very_poor" name="very_poor">$0-$100,000</option>
+    <option value="poor" name="poor">$100,000-$500,000</option>
+    <option value="good" name="good">$500,000-$1,000,000</option>
+    <option value="great" name="great">$1,000,000-$10,000,000</option>
+  </select>
+  <button type="submit" value="Submit"> Sort</buttn>
+</form>
+</div>
+
 <br>
 _END;
 // connect directly to the database:
