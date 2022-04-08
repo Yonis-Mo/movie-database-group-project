d40b93083e3137fd61c7c904de6c43dc8be0a6ef
Redda99 <102543458+Redda99@users.noreply.github.com>
2022-04-08 13:04:40 +0100

    basick html psuedo code

--- header.php
+++ header.php
@@ -7,3 +7,22 @@
 // sign in page
 // only home page should be live for now...
 ?>
+
+<!DOCTYPE html>
+ <header>
+    <a href=index.html>
+        <img src="red.png"100" height="100">
+    </a>
+    <nav>
+        <ul>
+         <li><a href=index.html>Home</a>
+         <li><a href=about.html>about</a>
+         <li><a href=contact.html>contact</a>
+        </ul>
+    </nav>
+
+</header>
+<h1>home page</h1>
+<footer>
+    @2022 my name
+</footer>
