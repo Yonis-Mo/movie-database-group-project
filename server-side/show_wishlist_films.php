e172f4bc2d57c14dd76b5973a9c80135f13d1762
ChickenBoy1 <liamsingh64@gmail.com>
2022-04-08 14:25:31 +0100

    Shows Movie Data In Table

--- server-side/show_wishlist_films.php
+++ server-side/show_wishlist_films.php
@@ -2,7 +2,6 @@
 
 require_once('credentials.php');
 
-// $sql = "SELECT REPLACE(REPLACE(REPLACE(Wishlist, '\"', ''), '[', ''), ']', '') AS Wishlist FROM users WHERE ID = ?;";
 $sql = "SELECT REPLACE(REPLACE(REPLACE(Wishlist, '\"', ''), '[', '('), ']', ')') AS Wishlist FROM users WHERE ID = ?;";
 $stmt = $conn->prepare($sql);
 
@@ -12,7 +11,18 @@ $stmt->bind_param("i", $id);
 $stmt->execute();
 $result = $stmt->get_result();
 
+$table = <<<TABLE
+<table>
+  <th>Title</th>
+  <th>Overview</th>
+  <th>Budget</th>
+  <th>Revenue</th>
+  <th>Release Date</th>
+  <th>Runtime</th>
+  <th>Average Vote</th>
+  <th>Popularity</th>
 
+TABLE;
 
 if($result->num_rows > 0){
     //echo $table;
@@ -21,9 +31,25 @@ if($result->num_rows > 0){
         $stmt = $conn->prepare($sql);
         $stmt->execute();
         $result = $stmt->get_result();
-        while ($row = $result->fetch_assoc()) {
-            echo $row['Title'];
-        }
+
+        echo $table;
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>".$row['Title']."</td>";
+                echo "<td>".$row['Overview']."</td>";
+                echo "<td>".$row['Budget']."</td>";
+                echo "<td>".$row['Revenue']."</td>";
+            
+                $UNIX_DATE = ($row['ReleaseDate'] - 25569) * 86400;
+                $readable_date = gmdate("d-m-Y H:i:s", $UNIX_DATE);
+                $readable_date = substr($readable_date, 0, 10);
+                echo "<td>".$readable_date."</td>";
+                echo "<td>".$row['Runtime']."</td>";
+                echo "<td>".$row['VoteAvg']."</td>";
+                echo "<td>".$row['Popularity']."</td>";
+                echo "</tr>";        
+            }
+        echo "</table>";
     }
 }else{
     echo "User Does Not Have Any Films In Their Wishlist";
