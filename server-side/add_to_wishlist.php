74c0574777354f4be5f3572c18782804a60267f4
ChickenBoy1 <liamsingh64@gmail.com>
2022-04-08 13:51:47 +0100

    Can Add To Wishlist
    No $_GET variables yet!

---
+++ server-side/add_to_wishlist.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once('credentials.php');
+
+$new_film_to_wishlist = 3;
+
+$sql = "SELECT * FROM users WHERE ID = ?;";
+$stmt = $conn->prepare($sql);
+
+$id = 1;
+$stmt->bind_param("i", $id);
+
+$stmt->execute();
+$result = $stmt->get_result();
+
+
+if($result->num_rows > 0){
+    $new_wishlist = array();
+    while ($row = $result->fetch_assoc()) {
+
+        $current_wishlist = json_decode($row['Wishlist']);
+
+        if(is_null($current_wishlist)){
+
+            echo "No Wishlist";
+            array_push($new_wishlist, $new_film_to_wishlist);
+        }else{
+            for($i = 0; $i < count($current_wishlist); $i++){
+                array_push($new_wishlist, $current_wishlist[$i]);
+
+            }
+            array_push($new_wishlist, $new_film_to_wishlist);
+
+        }
+    }
+
+}else{
+  echo "Sorry! We couldn't find any films of that genre";
+}
+
+$sql = "UPDATE users SET Wishlist = ? WHERE ID = ?;";
+$stmt = $conn->prepare($sql);
+
+$new_wishlist = json_encode($new_wishlist);
+echo($new_wishlist);
+
+$stmt->bind_param("si", $new_wishlist, $id);
+
+if($stmt->execute()){
+    echo "Wishlist Added";
+}else{
+    echo "Error Adding Wishlist";
+}
+
+
+
+
+
+
+
+?>
+
