9fcf2e3b173bf186af0bc50b57c0b64315985d41
Redda99 <102543458+Redda99@users.noreply.github.com>
2022-04-08 13:08:13 +0100

    basic html pusedo code

--- login.php
+++ login.php
@@ -2,3 +2,31 @@
 //sessions stored should be user, admins, "logged in"
 //form to login
 ?>
+
+
+
+
+<!DOCTYPE html>
+				
+				<label> Username: 
+					<input id="username" type="text" name="username">			
+				</label><br>
+				<label> <br>
+				 Password: 
+					<input id="password" type="password" name="password">			
+				</label><br>
+								
+				 <br/>
+				 
+				 <br>
+				 
+				<input type="submit" value="Login" />
+				<br />
+					
+					
+		
+		
+
+
+
+</html>
