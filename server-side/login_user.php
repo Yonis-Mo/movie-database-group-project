65a0389510d77f697017d128d335ced9dbe0ef81
ChickenBoy1 <liamsingh64@gmail.com>
2022-04-08 12:25:35 +0100

    User can be found
    Not added $_GET variables

---
+++ server-side/login_user.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('credentials.php');
+
+$sql = "SELECT * FROM users WHERE Username = ? AND Password = ?;";
+$stmt = $conn->prepare($sql);
+
+$uname = "admin";
+$pword = "admin";
+
+$stmt->bind_param("ss", $uname, $pword);
+
+$stmt->execute();
+$result = $stmt->get_result();
+
+$table = <<<TABLE
+<table>
+  <th>ID</th>
+  <th>Username</th>
+  <th>Password</th>
+  <th>Firstname</th>
+  <th>Lastname</th>
+  <th>Wishlist</th>
+TABLE;
+
+
+if($result->num_rows > 0){
+  echo $table;
+while ($row = $result->fetch_assoc()) {
+  
+    echo "<tr>";
+    echo "<td>".$row['ID']."</td>";
+    echo "<td>".$row['Username']."</td>";
+    echo "<td>".$row['Password']."</td>";
+    echo "<td>".$row['Firstname']."</td>";
+    echo "<td>".$row['Lastname']."</td>";
+    echo "<td>".$row['Wishlist']."</td>";
+    echo "</tr>";
+
+}
+}else{
+  echo "Sorry! We couldn't find any films of that genre";
+}
+
+echo "</table>";
+
+?>
+
